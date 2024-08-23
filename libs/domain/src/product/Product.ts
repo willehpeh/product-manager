ecf5f3bce2faf8d0ff35011cbeb268a427127553
w.alexander <will_alexander@me.com>
2024-08-23 15:13:49 +0200

    Give products a name

--- libs/domain/src/product/Product.ts
+++ libs/domain/src/product/Product.ts
@@ -1,16 +1,20 @@
 import { AggregateRoot } from '../shared/AggregateRoot';
 import { ProductId } from './ProductId';
+import { ProductName } from './ProductName';
 
 export class Product extends AggregateRoot<ProductId> {
-  private readonly _id: ProductId;
 
-  constructor(id: ProductId) {
+  private readonly _id: ProductId;
+  private _name: ProductName;
+
+  constructor(id: ProductId, name: ProductName) {
     super();
     this._id = id;
+    this._name = name;
   }
 
-  is(entity: Product): boolean {
-    return this._id.equals(entity._id);
+  is(other: Product): boolean {
+    return this._id.equals(other._id);
   }
 
 }
